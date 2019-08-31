cb134987bc38b38c497b93d268a12bf55d13021c
financiali <ali@freelancer.run>
2019-08-31 22:57:33 +0100

    - bug fix

--- nodes/click-element/click-element.js
+++ nodes/click-element/click-element.js
@@ -8,9 +8,14 @@ module.exports = function (RED) {
 
 
         var node = this;
+        var retry_count = 0;
         var timer;
+        var timeout_timer;
+        var sended = false;
+
+
         node.on('input', function (msg) {
-            node.status({fill: "yellow", shape: "dot", text: 'clicking...'});
+
             var server_address = msg.server_address || msg.payload.server_address || null;
             if (!server_address)
                 return node.error('server_address id required', msg);
@@ -26,26 +31,77 @@ module.exports = function (RED) {
 
             var url = server_address + '/wd/hub/session/' + appium_session_id + '/element/' + element_id + '/click';
 
-            request.post({
-                url: url,
-                json: {}
-            }, function (e, r, body) {
-                if (e) {
-                    node.error(e, msg);
-                    node.status({fill: "red", shape: "ring", text: "e"});
-                } else if (r.statusCode !== 200) {
-                    // node.error(body.value.message, msg);
-                    node.status({fill: "red", shape: "ring", text: body.value.message});
-                } else if (body.value !== true) {
-                    // node.error(body.value.message, msg);
-                    node.status({fill: "red", shape: "ring", text: body.value.message});
-                } else {
-                    // msg.payload = {};
-                    node.status({fill: "green", shape: "ring", text: 'Clicked'});
+
+            config.retry_limit = 2;
+
+            if (typeof config.retry_limit === "undefined" || !config.retry_limit)
+                config.retry_limit = 3;
+
+            config.retry_limit = parseInt(config.retry_limit);
+
+            timeoutTimer(function () {
+                if (!sended) {
+                    sended = true;
+                    node.status({fill: "orange", shape: "dot", text: 'timeout'});
                     timerStatus();
-                    node.send(msg);
+                    node.send([null, msg]);
                 }
             });
+
+            var call = function () {
+                if (sended) return;
+
+                node.status({fill: "yellow", shape: "dot", text: 'clicking...'});
+                request.post({
+                    url: url,
+                    json: {}
+                }, function (e, r, body) {
+                    if (e) {
+
+                        if (retry_count >= config.retry_limit) {
+                            node.error(e, msg);
+                            node.send([null, msg]);
+                        } else {
+                            node.status({fill: "orange", shape: "dot", text: 'retrying'});
+                            call();
+                        }
+
+                    } else if (r.statusCode !== 200) {
+                        // node.error(body.value.message, msg);
+                        // node.status({fill: "red", shape: "ring", text: body.value.message});
+                        if (retry_count >= config.retry_limit) {
+                            node.warn(body.value.message, msg);
+                            node.status({fill: "red", shape: "ring", text: body.value.message});
+                            node.send([null, msg]);
+                        } else {
+                            node.status({fill: "orange", shape: "dot", text: 'retrying'});
+                            call();
+                        }
+
+                    } else if (body.value !== true) {
+                        // node.error(body.value.message, msg);
+                        // node.status({fill: "red", shape: "ring", text: body.value.message});
+
+                        if (retry_count >= config.retry_limit) {
+                            node.warn(body.value.message, msg);
+                            node.status({fill: "red", shape: "ring", text: body.value.message});
+                            node.send([null, msg]);
+                        } else {
+                            node.status({fill: "orange", shape: "dot", text: 'retrying'});
+                            call();
+                        }
+
+                    } else {
+                        // msg.payload = {};
+                        sended = true;
+                        node.status({fill: "green", shape: "ring", text: 'Clicked'});
+                        node.send([msg]);
+                    }
+                    timerStatus();
+                });
+            };
+            call();
+
         });
 
         node.on("close", function (done) {
@@ -57,6 +113,13 @@ module.exports = function (RED) {
                 clearTimeout(timer);
                 node.status({});
             }, 1000);
+        };
+
+        var timeoutTimer = function (cb) {
+            timeout_timer = setTimeout(function () {
+                clearTimeout(timeout_timer);
+                cb();
+            }, 10 * 1000);
         };
     }
 
