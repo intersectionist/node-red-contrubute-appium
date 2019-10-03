c9e8f8edd9efd9c6becbc8343f704bd38d386ce5
financiali <ali@freelancer.run>
2019-10-03 20:58:17 +0100

    - implement screen recording

--- nodes/click-element/click-element.js
+++ nodes/click-element/click-element.js
@@ -13,18 +13,18 @@ module.exports = function (RED) {
 
             var server_address = msg.server_address || msg.payload.server_address || null;
             if (!server_address) {
-                return node.send([null, null]);
+                return node.send([null, true]);
             }
 
             var appium_session_id = msg.appium_session_id || msg.payload.appium_session_id || null;
             if (!appium_session_id) {
-                return node.send([null, null]);
+                return node.send([null, true]);
             }
 
 
             var element_id = msg.element_id || msg.payload.element_id || null;
             if (!element_id) {
-                return node.send([null, null]);
+                return node.send([null, true]);
             }
 
             var url = server_address + '/wd/hub/session/' + appium_session_id + '/element/' + element_id + '/click';
@@ -32,14 +32,21 @@ module.exports = function (RED) {
             var call = function () {
 
 
+                console.log(server_address)
+
                 node.status({fill: "yellow", shape: "dot", text: 'clicking...'});
                 try {
                     request.post({
                         url: url,
                         json: {}
                     }, function (e, r, body) {
-                        if (e) {
 
+
+                        console.log('e',e)
+                        console.log('r',r)
+                        console.log('body',body)
+
+                        if (e) {
                             // node.error(e, msg);
                             node.send([null, msg]);
 
