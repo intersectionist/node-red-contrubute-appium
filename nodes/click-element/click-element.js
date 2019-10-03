f4751ed74202c16da1d0990347a581cdf9362b34
financiali <ali@freelancer.run>
2019-10-03 20:34:15 +0100

    - implement screen recording

--- nodes/click-element/click-element.js
+++ nodes/click-element/click-element.js
@@ -15,16 +15,16 @@ module.exports = function (RED) {
 
             var server_address = msg.server_address || msg.payload.server_address || null;
             if (!server_address)
-                return node.error('server_address id required', msg);
+                return node.warn('server_address id required', msg);
 
             var appium_session_id = msg.appium_session_id || msg.payload.appium_session_id || null;
             if (!appium_session_id)
-                return node.error('appium_session_id id required', msg);
+                return node.warn('appium_session_id id required', msg);
 
 
             var element_id = msg.element_id || msg.payload.element_id || null;
             if (!element_id)
-                return node.error('element_id id required', msg);
+                return node.warn('element_id id required', msg);
 
             var url = server_address + '/wd/hub/session/' + appium_session_id + '/element/' + element_id + '/click';
 
@@ -32,37 +32,41 @@ module.exports = function (RED) {
                 if (sended) return;
 
                 node.status({fill: "yellow", shape: "dot", text: 'clicking...'});
-                request.post({
-                    url: url,
-                    json: {}
-                }, function (e, r, body) {
-                    if (e) {
+                try {
+                    request.post({
+                        url: url,
+                        json: {}
+                    }, function (e, r, body) {
+                        if (e) {
 
-                        node.error(e, msg);
-                        node.send([null, msg]);
+                            // node.error(e, msg);
+                            node.send([null, msg]);
 
-                    } else if (r.statusCode !== 200) {
-                        // node.error(body.value.message, msg);
-                        // node.status({fill: "red", shape: "ring", text: body.value.message});
-                        node.warn(body.value.message, msg);
-                        node.status({fill: "red", shape: "ring", text: body.value.message});
-                        node.send([null, msg]);
+                        } else if (r.statusCode !== 200) {
+                            // node.error(body.value.message, msg);
+                            // node.status({fill: "red", shape: "ring", text: body.value.message});
+                            node.warn(body.value.message, msg);
+                            node.status({fill: "red", shape: "ring", text: body.value.message});
+                            node.send([null, msg]);
 
-                    } else if (body.value !== true) {
-                        // node.error(body.value.message, msg);
-                        // node.status({fill: "red", shape: "ring", text: body.value.message});
+                        } else if (body.value !== true) {
+                            // node.error(body.value.message, msg);
+                            // node.status({fill: "red", shape: "ring", text: body.value.message});
 
-                        node.warn(body.value.message, msg);
-                        node.status({fill: "red", shape: "ring", text: body.value.message});
-                        node.send([null, msg]);
+                            node.warn(body.value.message, msg);
+                            node.status({fill: "red", shape: "ring", text: body.value.message});
+                            node.send([null, msg]);
 
-                    } else {
-                        // msg.payload = {};
-                        sended = true;
-                        node.status({fill: "green", shape: "ring", text: 'Clicked'});
-                        node.send([msg]);
-                    }
-                });
+                        } else {
+                            // msg.payload = {};
+                            sended = true;
+                            node.status({fill: "green", shape: "ring", text: 'Clicked'});
+                            node.send([msg]);
+                        }
+                    });
+                }catch (e) {
+
+                }
             };
             call();
 
