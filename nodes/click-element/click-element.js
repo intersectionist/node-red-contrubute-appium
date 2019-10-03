b5b2c8a13366a7ea3ce842ce4b3a83c0c351fb57
financiali <ali@freelancer.run>
2019-10-03 20:48:21 +0100

    - implement screen recording

--- nodes/click-element/click-element.js
+++ nodes/click-element/click-element.js
@@ -14,17 +14,20 @@ module.exports = function (RED) {
 
 
             var server_address = msg.server_address || msg.payload.server_address || null;
-            if (!server_address)
-                return node.warn('server_address id required', msg);
+            if (!server_address) {
+                node.send([null, null]);
+            }
 
             var appium_session_id = msg.appium_session_id || msg.payload.appium_session_id || null;
-            if (!appium_session_id)
-                return node.warn('appium_session_id id required', msg);
+            if (!appium_session_id) {
+                node.send([null, null]);
+            }
 
 
             var element_id = msg.element_id || msg.payload.element_id || null;
-            if (!element_id)
-                return node.warn('element_id id required', msg);
+            if (!element_id) {
+                node.send([null, null]);
+            }
 
             var url = server_address + '/wd/hub/session/' + appium_session_id + '/element/' + element_id + '/click';
 
@@ -64,12 +67,13 @@ module.exports = function (RED) {
                             node.send([msg]);
                         }
                     });
-                }catch (e) {
+                    node.status({fill: "green", shape: "dot", text: 'clicked'});
+                } catch (e) {
+                    node.status({fill: "yellow", shape: "dot", text: 'error'});
                     node.send([null, e]);
                 }
             };
             call();
-
 
 
         });
